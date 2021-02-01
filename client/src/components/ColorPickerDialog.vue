f42a4a7a61582be6ed77fcaa3a63fd67f6e4baa4
june <junearagoncillo@gmail.com>
2021-02-01 15:53:41 +0800

    dialog color picker

---
+++ client/src/components/ColorPickerDialog.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-dialog
+      v-model="show"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Use Google's location service?
+        </v-card-title>
+        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="show = false"
+          >
+            Disagree
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="show = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+<script>
+export default {
+    props: ['visible'],
+    computed: {
+    show: {
+      get () {
+        return this.visible
+      },
+      set (value) {
+        if (!value) {
+          this.$emit('close')
+        }
+      }
+    }
+  }
+}
+</script>
