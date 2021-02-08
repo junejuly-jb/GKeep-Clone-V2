119b65215b39d2e3c0def4eb8cc5c24d13dc00c6
june <junearagoncillo@gmail.com>
2021-02-08 14:29:21 +0800

    skeleton loader add on

---
+++ client/src/components/Loader.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+            <div class="col">
+                <v-skeleton-loader
+                type="article, actions"
+                ></v-skeleton-loader>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    // data: function(){
+    //     return{
+    //         skeleton: false
+    //     }
+    // }
+}
+</script>
+<style scoped>
+
+</style>
